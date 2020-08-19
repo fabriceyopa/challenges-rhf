2396289cbc4f9ef186f50ed694369f0f96dd1ada
Fabrice Yopa <fabrice_yopa@yahoo.fr>
2020-08-19 15:41:27 +0100

    create all components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Step1 from "./pages/Step1";
+import Step2 from "./pages/Step2";
+import Step3 from "./pages/Step3";
+import Result from "./pages/Result";
+import { Header } from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Step1} />
+          <Route path="/step2" component={Step2} />
+          <Route path="/step3" component={Step3} />
+          <Route path="/result" component={Result} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Step1.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { useData } from "../DataContext";
+import Typography from "@material-ui/core/Typography";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers";
+import { PrimaryButton } from "../components/PrimaryButton";
+import { MainContainer } from "../components/MainContainer";
+import { Form } from "../components/Form";
+import { Input } from "../components/Input";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  firstName: yup
+    .string()
+    .matches(/^([^0-9]*)$/, "First name should not contain numbers")
+    .required("First name is a required field"),
+  lastName: yup
+    .string()
+    .matches(/^([^0-9]*)$/, "Last name should not contain numbers")
+    .required("Last name is a required field"),
+});
+
+const Step1 = () => {
+  const { setValues, data } = useData();
+  const history = useHistory();
+  const { register, handleSubmit, errors } = useForm({
+    defaultValues: { firstName: data.firstName, lastName: data.lastName },
+    mode: "onBlur",
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    history.push("./step2");
+    setValues(data);
+  };
+
+  return (
+    <MainContainer>
+      <Typography component="h2" variant="h5">
+        <span role="img" aria-label="Unicome" aria-describedby="name">
+          🦄
+        </span>{" "}
+        Step 1
+      </Typography>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          ref={register}
+          id="firstName"
+          type="text"
+          label="First Name"
+          name="firstName"
+          error={!!errors.firstName}
+          helperText={errors?.firstName?.message}
+        />
+        <Input
+          ref={register}
+          id="lastName"
+          type="text"
+          label="Last Name"
+          name="lastName"
+          error={!!errors.lastName}
+          helperText={errors?.lastName?.message}
+        />
+        <PrimaryButton>Next</PrimaryButton>
+      </Form>
+    </MainContainer>
+  );
+};
+
+export default Step1;
